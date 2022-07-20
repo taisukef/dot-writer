a14dc74d0ad6f9f7ff52532207ce2cf799acfe39
Taisuke Fukuno <fukuno@jig.jp>
2022-07-21 06:46:54 +0900

    move 'make' to 'index'

--- src/app/routed/home/pages/index/index.component.ts
+++ src/app/routed/home/pages/index/index.component.ts
@@ -1,14 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { DotEditorComponent } from '../../components/dot-editor/dot-editor.component';
 
 @Component({
   templateUrl: './index.component.html',
   standalone: true,
+  styleUrls: ['index.component.scss'],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    DotEditorComponent
   ]
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit{
+  constructor (
+  ) {}
+
+
+  ngOnInit (): void {
+  }
 }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {path: '', loadChildren: () => import('./routed/home/routes').then(mod => mod.ROUTES)},
-    {path: 'make', loadChildren: () => import('./routed/make/routes').then(mod => mod.ROUTES)},
   ];
   
